e556ae5b0e0eb08a327eaf7080cc7c67c6afd8e4
zhijl <384403717@qq.com>
2019-01-18 10:31:02 +0800

    Fixed bug

--- codes/c/961.c
+++ codes/c/961.c
@@ -10,8 +10,8 @@ int repeatedNTimes(int* A, int ASize) {
   if (ASize <= 2) {
     return A[0];
   }
-  for (int i = 2; i < ASize; ++i) {
-    if (A[i] == A[i-1] || A[i] == A[i-2]) {
+  for (int i = 3; i < ASize; ++i) {
+    if (A[i] == A[i-1] || A[i] == A[i-2] || A[i] == A[i-3]) {
       return A[i];
     }
   }
@@ -22,9 +22,11 @@ int main() {
   int A1[10] = {1, 2, 10, 10, 5, 10, 7, 10, 9, 10};
   int A2[10] = {1, 2, 10, 10, 5, 10, 7, 9, 10, 10};
   int A3[10] = {1, 10, 2, 10, 5, 10, 7, 10, 8, 10};
+  int A4[4] = {9, 5, 6, 9};
   printf("%d\n", repeatedNTimes(A1, 10));
   printf("%d\n", repeatedNTimes(A2, 10));
   printf("%d\n", repeatedNTimes(A3, 10));
+  printf("%d\n", repeatedNTimes(A4, 4));
   return 0;
 }
 
