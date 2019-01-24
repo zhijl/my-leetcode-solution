58478a5b5d29176c5f6f19405d2388392318b63f
zhijl <384403717@qq.com>
2019-01-24 10:14:26 +0800

    Update

--- codes/c/905.c
+++ codes/c/905.c
@@ -6,19 +6,16 @@
 int* sortArrayByParity(int* A, int ASize, int* returnSize) {
   int* out_arr = NULL;
   out_arr = (int*)malloc(ASize * sizeof(int));
+  int m = 0;
+  int n = ASize-1;
   int i = 0;
-  int j = 0;
   for (; i < ASize; ++i) {
     if (A[i] % 2 == 0) {
-      out_arr[j] = A[i];
-      ++j;
-    }
-  }
-  i = 0;
-  for (; i < ASize; ++i) {
-    if (A[i] % 2 != 0) {
-      out_arr[j] = A[i];
-      ++j;
+      out_arr[m] = A[i];
+      ++m;
+    } else {
+      out_arr[n] = A[i];
+      --n;
     }
   }
   *returnSize = ASize;
